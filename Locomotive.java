b06834e041acec68354d7ae98aa359799d87eb89
borzuy2282 <tim.barzashvili@gmail.com>
2023-03-17 01:33:02 +0100

    modified class Locomotive, added getters and setters.

--- Locomotive.java
+++ Locomotive.java
@@ -1,4 +1,7 @@
+
+
 public class Locomotive {
+    //variables:
     private String name;
     private Station home;
     private double currentSpeed;
@@ -9,10 +12,63 @@ public class Locomotive {
     private String idLocomotive;
     private static int forId = 0;
 
+    //constructor:
     Locomotive(String n, Station h){
         this.name = n;
         this.home = h;
         this.from = h;
-        this.idLocomotive = "L" +
+        this.idLocomotive = "L" + (++forId);
+    }
+    //getters:
+    public String getName() {
+        return name;
+    }
+
+    public String getIdLocomotive() {
+        return idLocomotive;
+    }
+
+    public double getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public Station getHome() {
+        return home;
+    }
+
+    public Station getGlobalFrom() {
+        return globalFrom;
+    }
+
+    public Station getFrom() {
+        return from;
+    }
+
+    public Station getTo() {
+        return to;
+    }
+
+    public Station getGlobalTo() {
+        return globalTo;
+    }
+    //setters:
+    public void setGlobalFrom(Station globalFrom) {
+        this.globalFrom = globalFrom;
+    }
+
+    public void setFrom(Station from) {
+        this.from = from;
+    }
+
+    public void setTo(Station to) {
+        this.to = to;
+    }
+
+    public void setGlobalTo(Station globalTo) {
+        this.globalTo = globalTo;
+    }
+
+    public void setCurrentSpeed(double currentSpeed) {
+        this.currentSpeed = currentSpeed;
     }
 }
