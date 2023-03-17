e744472ae937416c8a85ea1554d7f88ad382a3ab
borzuy2282 <tim.barzashvili@gmail.com>
2023-03-17 08:20:13 +0100

    Changed Car class to abstract class, added common variables, constructor and getters.

--- Car.java
+++ Car.java
@@ -1,4 +1,17 @@
-public interface Car {
-    public String getType();
-    public String getId();
+abstract public class Car {
+    private String name;
+    public static int forId = 0;
+    private String carId;
+    Car(String n){
+        this.name = n;
+        this.carId = "C" + (++forId);
+    }
+    //getters:
+    public String getName() {
+        return name;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
 }
